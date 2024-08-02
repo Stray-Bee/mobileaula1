11c1b41848b150df0a2c111fb355f780abde59db
Valhalla <valhallamaia@gmail.com>
2024-08-02 20:23:57 -0300

    novos prints

--- teste.java
+++ teste.java
@@ -2,6 +2,8 @@ package mobileaula1;
 
 public class teste {
     public static void main (String[] args){
+        System.out.print("123123");
         System.out.print("Olá Mundo!");
+        System.out.print("123123");
     }
 }
